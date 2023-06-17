c3fa6d9376399bbfa078830a3843387e1b25f2b0
huangbo <1104133609@qq.com>
2023-06-17 19:04:54 +0800

    refactor(custom): husky 新增

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
-import uniTwuCssPlugin from '@uni-helper/vite-plugin-uni-tailwind'
+import Unocss from 'unocss/vite'
+
 export default defineConfig({
-  plugins: [uni(), uniTwuCssPlugin()],
-  css: {
-    postcss: {
-      plugins: [require('tailwindcss')()],
-    },
-  },
+  plugins: [uni(), Unocss()]
 })
